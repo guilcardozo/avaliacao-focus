03e7ed862f09e0477b5a2b9a487c5256224598eb
guilcardozo <garaujo.cardozo@gmail.com>
2019-01-25 19:12:06 -0200

    Add files via upload

---
+++ assets/js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slider').slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  adaptiveHeight: true
+  });
+});
